61bc559bf28f0ada6b45b7aa64c9c9affcf86daf
Karim Oulad Chalha <herr.linux88@gmail.com>
2016-01-23 22:29:11 +0000

    nothing new

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
-'use strict'
+'use strict';
 
 var app = angular.module('app', []);
 
 
-app.controller('GameController', ['scope', function($scope){
+app.controller('GameController', ['$scope', function($scope){
   $scope.name = 'Turn Them Off!';
 }]);
